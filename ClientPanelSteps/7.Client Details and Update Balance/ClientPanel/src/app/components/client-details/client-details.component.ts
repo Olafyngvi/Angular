7016884948717e46645c6633645c3c4d9b20ad78
Rijad Dzanko <rijadda@gmail.com>
2020-01-24 15:22:36 +0100

    Šesti

---
+++ ClientPanelSteps/7.Client Details and Update Balance/ClientPanel/src/app/components/client-details/client-details.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { Client } from '../../models/Client';
+
+@Component({
+  selector: 'app-client-details',
+  templateUrl: './client-details.component.html',
+  styleUrls: ['./client-details.component.css']
+})
+export class ClientDetailsComponent implements OnInit {
+id: string;
+client: Client;
+// tslint:disable-next-line: no-inferrable-types
+hasBalance: boolean = false;
+// tslint:disable-next-line: no-inferrable-types
+showBalanceUpdateInput: boolean = false;
+
+  constructor(private flasMessage: FlashMessagesService,
+              private clientService: ClientService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // Get id from url
+    this.id = this.route.snapshot.params.id;
+    // Get client
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client != null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      this.client = client;
+    });
+  }
+  updateBalance() {
+    this.clientService.updateClient(this.client);
+    this.flasMessage.show('Balance updated', {cssClass: 'alert-success', timeout: 4000});
+  }
+}
